c43819ee116ae5211df2bd54710d4ab1e1fd3b4e
Mouy KimHong <khong67663@gmail.com>
2023-11-09 14:33:05 +0700

    add welcome

--- src/component/MyNavBar.js
+++ src/component/MyNavBar.js
@@ -9,6 +9,7 @@ function MyNavBar() {
     <Navbar expand="lg" className="bg-body-tertiary p-3">
       <Container>
         <Navbar.Brand href="/"><FaPagelines/> Create Product</Navbar.Brand>
+        <h1>welcome to my shop</h1>
 
 
       <div className="languague-picker">
