8adca9678d78591f1497bb843382a12d4a9905f8
Vorn sama <Vornkh87@gmail.com>
2023-11-21 07:10:44 +0700

    vorn

--- src/service/constants/ApiUrl.js
+++ src/service/constants/ApiUrl.js
@@ -3,7 +3,8 @@
     export  const Mainurl = "http://vorn.ponlue.bio:5046/api/product?search="
     export  const SortUrl = "http://vorn.ponlue.bio:5046/api/product?ordering="
     export const CategoryUrl = "http://vorn.ponlue.bio:5046/api/category"
-
+    export const CategoryUrlCreate = "http://vorn.ponlue.bio:5046/api/category/create"
+    export const UploadImage = "http://vorn.ponlue.bio:5046/api/image";
     export const LoginUrl = "http://vorn.ponlue.bio:5046/api/login"
     export const LogoutUrl = "http://vorn.ponlue.bio:5046/api/logout"
     export const SignUpUrl = "http://vorn.ponlue.bio:5046/api/register"
