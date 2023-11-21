c52c9d290ee6c79b8e6bdb69d4a228bb9b9f8782
Sovan panhavorn <nightpp19@gmail.com>
2023-11-21 08:05:43 +0700

    run

--- src/service/action/Category.js
+++ src/service/action/Category.js
@@ -1,4 +1,12 @@
+<<<<<<< Updated upstream
 export const fetccate = async  (url) =>{
+=======
+
+
+
+
+export const fetchallcategory = async  (url) =>{
+>>>>>>> Stashed changes
 
     const res = await fetch(`${url}`)
  
